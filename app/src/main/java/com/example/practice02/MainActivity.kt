ff9480447525e4ed37d108476a40bdac14430dfe
kangyuri1114 <yuri3334@gmail.com>
2023-03-03 17:12:04 +0900

    commit 3 목표 시간 랜덤으로 정하기

--- app/src/main/java/com/example/practice02/MainActivity.kt
+++ app/src/main/java/com/example/practice02/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.lang.Math.abs
 import java.util.Timer
 import kotlin.concurrent.timer
 
@@ -12,32 +13,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)//레이아웃의 액티비티 메인 xml파일을 불러옴
         var sec: Int = 0
-        var isRunning = false //초기에 실행되고 있지 않음
+        var isRunning = false //초기에 실행 되고 있지 않음
         var timerTask: Timer? = null  //nullable로 선언
-        val tv: TextView = findViewById(R.id.tv_hello)
-        val btn: Button = findViewById(R.id.btn_kor)
+        val tv_t: TextView = findViewById(R.id.tv_timer)
+        val tv_r: TextView = findViewById(R.id.tv_random)
+        val tv_p: TextView = findViewById(R.id.tv_point)
+
+
+        val btn: Button = findViewById(R.id.btn_main)
+
+        //랜덤 수 생성하기 박스 변수
+        val random_box = java.util.Random()
+        val num = random_box.nextInt(1001) //11을 넣으면 0부터 10까지 정수로 반환
+        tv_r.text = (num.toFloat() / 100).toString()
+
 
         btn.setOnClickListener {
             isRunning = !isRunning //isRunning을 true로 바꾸어줌 클릭할때마다 false, true가 왔다갔다
             if (isRunning == true) {
-                timerTask = timer(period = 1000) { // period가 1000 mmsecond = 1초마다 함수가 돌아간다
+                timerTask = timer(period = 10) { // period가 1000 mmsecond = 1초마다 함수가 돌아간다
+                    //period가 10으로 설정, sec/100로 설정, sec의 자료형을 Int에서 float으로 변경 시 소수점까지 나오게 실행 가능
                     sec++
                     //UI를 바꾸기 위해서는 runOnUiThread를 사용해야 한다.
                     runOnUiThread { //실시간으로 화면을 바꾸기 위해 사용
-                        tv.text = sec.toString() //자료형 바꿔주기
+                        tv_t.text = (sec.toFloat() / 100).toString() //자료형 바꿔주기
                     }
                     //println(sec)  //초가 흘러갈때마다 시스템 상 출력
                 }
             } else {
                 timerTask?.cancel()  //nullable
+                val point = abs(sec-num).toFloat()/100 //절댓값 abs
+                tv_p.text = point.toString()
             }
+
         }
-
-
-//        val textView: TextView = findViewById(R.id.android_text) as TextView //변수 선언
-//        //val textView: TextView 에서 : TextView 는 textView가 어떤 객체인지 설명해주는
-//        textView.setOnClickListener {
-//            textView.text = getString(R.string.name) //문자열을 받아와라
-
     }
 }
