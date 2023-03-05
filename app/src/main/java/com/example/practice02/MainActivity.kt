07b5baf8230ab41fdf8f2b4ae0712ce36116c044
kangyuri1114 <yuri3334@gmail.com>
2023-03-05 23:39:33 +0900

    commit 4 시작과 종료 만들기

--- app/src/main/java/com/example/practice02/MainActivity.kt
+++ app/src/main/java/com/example/practice02/MainActivity.kt
@@ -9,28 +9,26 @@ import java.util.Timer
 import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)//레이아웃의 액티비티 메인 xml파일을 불러옴
-        var sec: Int = 0
-        var isRunning = false //초기에 실행 되고 있지 않음
+
+    fun main(){
+        setContentView(R.layout.activity_main)//리소스의 레이아웃의 액티비티 메인 xml파일(화면)을 불러옴
+        var stage = 1 //isRunning을 지우고 stage1,2,3...으로 전환, stage 1은 초기값
+        var sec: Int = 0 //기본 단위가 mmsec이므로 나누기 100을 해야 1초임
         var timerTask: Timer? = null  //nullable로 선언
         val tv_t: TextView = findViewById(R.id.tv_timer)
         val tv_r: TextView = findViewById(R.id.tv_random)
         val tv_p: TextView = findViewById(R.id.tv_point)
-
-
         val btn: Button = findViewById(R.id.btn_main)
 
         //랜덤 수 생성하기 박스 변수
-        val random_box = java.util.Random()
+        val random_box = java.util.Random() //변수
         val num = random_box.nextInt(1001) //11을 넣으면 0부터 10까지 정수로 반환
         tv_r.text = (num.toFloat() / 100).toString()
-
+        btn.text = "시작"
 
         btn.setOnClickListener {
-            isRunning = !isRunning //isRunning을 true로 바꾸어줌 클릭할때마다 false, true가 왔다갔다
-            if (isRunning == true) {
+            stage++ //클릭 시 stage가 올라감
+            if (stage == 2) {
                 timerTask = timer(period = 10) { // period가 1000 mmsecond = 1초마다 함수가 돌아간다
                     //period가 10으로 설정, sec/100로 설정, sec의 자료형을 Int에서 float으로 변경 시 소수점까지 나오게 실행 가능
                     sec++
@@ -40,12 +38,25 @@ class MainActivity : AppCompatActivity() {
                     }
                     //println(sec)  //초가 흘러갈때마다 시스템 상 출력
                 }
-            } else {
+                btn.text = "정지"
+            }
+            else if(stage == 3){
                 timerTask?.cancel()  //nullable
                 val point = abs(sec-num).toFloat()/100 //절댓값 abs
                 tv_p.text = point.toString()
+                btn.text = "다음"
+                stage = 0
+            }
+            else if(stage == 1){
+                main()//재귀함수
             }
 
         }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        main()
+        super.onCreate(savedInstanceState)
+
     }
 }
